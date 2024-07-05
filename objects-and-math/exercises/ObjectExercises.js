165a4db25b0b390dd30dd6223573e30d9a71ae5d
billkapes <kapes15@yahoo.com>
2024-07-04 22:38:08 -0500

    completed exercise

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -2,17 +2,77 @@ let superChimpOne = {
    name: "Chad",
    species: "Chimpanzee",
    mass: 9,
-   age: 6
+   age: 6,
+   astronautID: 1,
+   move: function() {
+      return Math.floor(Math.random()*11);
+   }
 };
 
 let salamander = {
    name: "Lacey",
    species: "Axolotl Salamander",
    mass: 0.1,
-   age: 5
+   age: 5,
+   astronautID: 2,
+   move: function() {
+      return Math.floor(Math.random()*11);
+   }
 };
 
+let chimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function() {
+      return Math.floor(Math.random()*11);
+   }
+};
 
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function() {
+      return Math.floor(Math.random()*11);
+   }
+};
+
+let tardigrade = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+   move: function() {
+      return Math.floor(Math.random()*11);
+   }
+};
+
+let crew = [superChimpOne, salamander, chimpTwo, dog, tardigrade];
+
+function crewReports(animal) {
+   console.log(`${animal.name} is a ${animal.species}. They are ${animal.age} years old and ${animal.mass} kilograms. Their ID is ${animal.astronautID}.`);
+}
+
+function fitnessTest(racers) {
+   let results = [], numSteps, turns;
+   for (let i=0; i<racers.length; i++) {
+      numSteps = 0;
+      turns = 0;
+      while(numSteps < 20) {
+         numSteps += racers[i].move();
+         turns++;
+      }
+      results.push(`${racers[i].name} took ${turns} turns to take 20 steps.`);
+   }
+   return results;
+}
+console.log(fitnessTest(crew));
 // After you have created the other object literals, add the astronautID property to each one.
 
 // Add a move method to each animal object
