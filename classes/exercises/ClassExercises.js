7e25cf642a8043ff44b552ad896ebf96a1378816
billkapes <kapes15@yahoo.com>
2024-07-18 01:32:13 -0500

    completed exercise

--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,10 +1,47 @@
-// Define your Book class here:
+class Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       this.title = title;
+       this.author = author;
+       this.copyright = copyright;
+       this.isbn = isbn;
+       this.pages = pages;
+       this.timesCheckedOut = timesCheckedOut;
+       this.discarded = discarded;
+    }
+ 
+    checkout(uses=1) {
+       this.timesCheckedOut += uses;
+    }
+ }
+ 
+ class Manual extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+ 
+    dispose(currentYear){
+       if (currentYear-this.copyright > 5) {
+          this.discarded = 'Yes';
+       }
+    }
+ }
+ 
+ class Novel extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+       super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+ 
+    dispose(){
+       if (this.timesCheckedOut > 100) {
+          this.discarded = 'Yes';
+       }
+    }
+ }
 
+ let novel1 = new Novel('Pride and Prejudice', 'Jane Austen', 1813, '1111111111111', 432, 32, 'No');
+ let manual1 = new Manual('Top Secret Shuttle Building Manual', 'Redacted', 2013, '0000000000000', 1147, 1, 'No');
 
-// Define your Manual and Novel classes here:
+ manual1.dispose(2024);
+ novel1.checkout(5);
 
-
-// Declare the objects for exercises 2 and 3 here:
-
-
-// Code exercises 4 & 5 here:
+ console.log(manual1, novel1);
