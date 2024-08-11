438570cb7afa34385dd5ada7f3b681c0a8b35e58
billkapes <kapes15@yahoo.com>
2024-08-11 13:06:12 -0500

    completed bonus mission

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,17 @@
 //Code Your Solution Below
+window.addEventListener("load", function() {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        let testnameInput = document.querySelector("input[name=testName]");
+        let testdateInput = document.querySelector("input[name=testDate]");
+        let boostercountInput = document.querySelector("input[name=boosterCount]");
+        //let windratingInput = document.querySelector("input[name=windRating]:checked");
+        //alert("wind ", windratingInput.value);
+
+        if (testnameInput.value === "" || testdateInput.value === "" || boostercountInput.value === "") {
+            alert("All fields are required.");
+            event.preventDefault();
+        }
+    
+    });
+});
